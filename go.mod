fed063c0dc9a5ebbe2f4abd199916a9311a6d37f
Eduardo Apolinario <eapolinario@users.noreply.github.com>
2023-10-25 15:55:39 -0700

    import flytestdlib
    Signed-off-by: Eduardo Apolinario <eapolinario@users.noreply.github.com>

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/eapolinario/golang-test-project
+
+go 1.19
+
+require github.com/flyteorg/flyte/flytestdlib v1.10.5-0.20231025215005-b30cb723afc7
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+import "github.com/flyteorg/flyte/flytestdlib/errors"
+
+const ErrRefreshingToken errors.ErrorCode = "TOKEN_REFRESH_FAILURE"
+
+func main() {
+  fmt.Println(ErrRefreshingToken)
+}
